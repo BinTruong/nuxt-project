7989f32b68e093cf173f4d53ad838fa0809815f3
BinTruong <bintruong97@gmail.com>
2021-09-25 11:12:56 +0700

    fix error stylelint

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,9 +1,15 @@
 module.exports = {
-  extends: [
-    'stylelint-config-standard',
-    'stylelint-config-prettier'
-  ],
+  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
-  rules: {}
+  rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['extends', 'tailwind'],
+      },
+    ],
+    'block-no-empty': null,
+    // 'unit-allowed-list': ['em', 'rem', 's', 'px'],
+  },
 }
